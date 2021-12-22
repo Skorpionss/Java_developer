e98f4b48efc1f758bb31c9fdbaa016041d94cf25
Maxim <Skorpionss>
2021-12-22 15:47:54 +0700

    Создали класс с заданными полями.

--- src/lesson_1_OOP/Person.java
+++ src/lesson_1_OOP/Person.java
@@ -1,4 +1,9 @@
 package lesson_1_OOP;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String country;
+    private String address;
 }

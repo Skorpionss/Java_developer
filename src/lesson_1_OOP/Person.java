6a2dab882cd86983a73fc2cfe22ebe90edacf3d3
Maxim <Skorpionss>
2021-12-22 18:54:31 +0700

    Добавил метод возвращающий готовый объект

--- src/lesson_1_OOP/Person.java
+++ src/lesson_1_OOP/Person.java
@@ -15,30 +15,37 @@ public class Person {
             newPerson = new Person();
         }
 
-        public Builder firstName(String name){
+        public Builder firstName(String name) {
             newPerson.firstName = name;
             return this;
         }
 
-        public Builder lastName(String name){
+        public Builder lastName(String name) {
             newPerson.lastName = name;
             return this;
         }
 
-        public Builder middleName(String name){
+        public Builder middleName(String name) {
             newPerson.middleName = name;
             return this;
         }
 
-        public Builder counter(String name){
+        public Builder counter(String name) {
             newPerson.country = name;
             return this;
         }
 
-        public Builder address(String name){
+        public Builder address(String name) {
             newPerson.address = name;
-            return  this;
+            return this;
         }
+
+        //Метод возвращающий готовый объект
+
+        public Person build() {
+            return newPerson;
+        }
+
 
     }
 }

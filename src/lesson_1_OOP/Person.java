3884b0c2aba70fa5cca26844682fef58bd3cadbc
Maxim <Skorpionss>
2021-12-22 16:03:43 +0700

    Создаем Bulder

--- src/lesson_1_OOP/Person.java
+++ src/lesson_1_OOP/Person.java
@@ -6,4 +6,39 @@ public class Person {
     private String middleName;
     private String country;
     private String address;
+
+
+    public static class Builder {
+        private Person newPerson;
+
+        public Builder() {
+            newPerson = new Person();
+        }
+
+        public Builder firstName(String name){
+            newPerson.firstName = name;
+            return this;
+        }
+
+        public Builder lastName(String name){
+            newPerson.lastName = name;
+            return this;
+        }
+
+        public Builder middleName(String name){
+            newPerson.middleName = name;
+            return this;
+        }
+
+        public Builder counter(String name){
+            newPerson.country = name;
+            return this;
+        }
+
+        public Builder address(String name){
+            newPerson.address = name;
+            return  this;
+        }
+
+    }
 }
